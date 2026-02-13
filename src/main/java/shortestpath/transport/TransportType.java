cc8ad144e02532bb55b3402f528c155ee82fa11a
Frans Irgolitsch <f.irgolitsch@outlook.com>
2026-02-13 13:48:02 -0500

    Swapped reflect for functional interface.

--- src/main/java/shortestpath/transport/TransportType.java
+++ src/main/java/shortestpath/transport/TransportType.java
@@ -2,11 +2,14 @@ package shortestpath.transport;
 
 import lombok.Getter;
 import net.runelite.api.Skill;
+import shortestpath.ShortestPathConfig;
+
+import java.util.function.Function;
 
 @Getter
 public enum TransportType {
     TRANSPORT("/transports/transports.tsv", null, null),
-    AGILITY_SHORTCUT("/transports/agility_shortcuts.tsv", "useAgilityShortcuts", "costAgilityShortcuts") {
+    AGILITY_SHORTCUT("/transports/agility_shortcuts.tsv", ShortestPathConfig::useAgilityShortcuts, ShortestPathConfig::costAgilityShortcuts) {
         @Override
         public TransportType refine(int[] skillLevels) {
             if (skillLevels[Skill.RANGED.ordinal()] > 1 || skillLevels[Skill.STRENGTH.ordinal()] > 1) {
@@ -15,64 +18,64 @@ public enum TransportType {
             return this;
         }
     },
-    GRAPPLE_SHORTCUT(null, "useGrappleShortcuts", "costGrappleShortcuts"),
-    BOAT("/transports/boats.tsv", "useBoats", "costBoats"),
-    CANOE("/transports/canoes.tsv", "useCanoes", "costCanoes"),
-    CHARTER_SHIP("/transports/charter_ships.tsv", "useCharterShips", "costCharterShips"),
-    SHIP("/transports/ships.tsv", "useShips", "costShips"),
-    FAIRY_RING("/transports/fairy_rings.tsv", "useFairyRings", "costFairyRings", 6),
-    GNOME_GLIDER("/transports/gnome_gliders.tsv", "useGnomeGliders", "costGnomeGliders", 6),
-    HOT_AIR_BALLOON("/transports/hot_air_balloons.tsv", "useHotAirBalloons", "costHotAirBalloons", 7),
-    MAGIC_CARPET("/transports/magic_carpets.tsv", "useMagicCarpets", "costMagicCarpets"),
-    MAGIC_MUSHTREE("/transports/magic_mushtrees.tsv", "useMagicMushtrees", "costMagicMushtrees", 5),
-    MINECART("/transports/minecarts.tsv", "useMinecarts", "costMinecarts"),
-    QUETZAL("/transports/quetzals.tsv", "useQuetzals", "costQuetzals"),
-    QUETZAL_WHISTLE("/transports/quetzal_whistle.tsv", "useQuetzals", "costQuetzalWhistle") {
+    GRAPPLE_SHORTCUT(null, ShortestPathConfig::useGrappleShortcuts, ShortestPathConfig::costGrappleShortcuts),
+    BOAT("/transports/boats.tsv", ShortestPathConfig::useBoats, ShortestPathConfig::costBoats),
+    CANOE("/transports/canoes.tsv", ShortestPathConfig::useCanoes, ShortestPathConfig::costCanoes),
+    CHARTER_SHIP("/transports/charter_ships.tsv", ShortestPathConfig::useCharterShips, ShortestPathConfig::costCharterShips),
+    SHIP("/transports/ships.tsv", ShortestPathConfig::useShips, ShortestPathConfig::costShips),
+    FAIRY_RING("/transports/fairy_rings.tsv", ShortestPathConfig::useFairyRings, ShortestPathConfig::costFairyRings, 6),
+    GNOME_GLIDER("/transports/gnome_gliders.tsv", ShortestPathConfig::useGnomeGliders, ShortestPathConfig::costGnomeGliders, 6),
+    HOT_AIR_BALLOON("/transports/hot_air_balloons.tsv", ShortestPathConfig::useHotAirBalloons, ShortestPathConfig::costHotAirBalloons, 7),
+    MAGIC_CARPET("/transports/magic_carpets.tsv", ShortestPathConfig::useMagicCarpets, ShortestPathConfig::costMagicCarpets),
+    MAGIC_MUSHTREE("/transports/magic_mushtrees.tsv", ShortestPathConfig::useMagicMushtrees, ShortestPathConfig::costMagicMushtrees, 5),
+    MINECART("/transports/minecarts.tsv", ShortestPathConfig::useMinecarts, ShortestPathConfig::costMinecarts),
+    QUETZAL("/transports/quetzals.tsv", ShortestPathConfig::useQuetzals, ShortestPathConfig::costQuetzals),
+    QUETZAL_WHISTLE("/transports/quetzal_whistle.tsv", ShortestPathConfig::useQuetzals, ShortestPathConfig::costQuetzalWhistle) {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    SEASONAL_TRANSPORTS("/transports/seasonal_transports.tsv", "useSeasonalTransports", "costSeasonalTransports"),
-    SPIRIT_TREE("/transports/spirit_trees.tsv", "useSpiritTrees", "costSpiritTrees", 5),
-    TELEPORTATION_BOX("/transports/teleportation_boxes.tsv", null, "costTeleportationBoxes"),
-    TELEPORTATION_ITEM("/transports/teleportation_items.tsv", null, "costNonConsumableTeleportationItems") {
+    SEASONAL_TRANSPORTS("/transports/seasonal_transports.tsv", ShortestPathConfig::useSeasonalTransports, ShortestPathConfig::costSeasonalTransports),
+    SPIRIT_TREE("/transports/spirit_trees.tsv", ShortestPathConfig::useSpiritTrees, ShortestPathConfig::costSpiritTrees, 5),
+    TELEPORTATION_BOX("/transports/teleportation_boxes.tsv", null, ShortestPathConfig::costTeleportationBoxes),
+    TELEPORTATION_ITEM("/transports/teleportation_items.tsv", null, ShortestPathConfig::costNonConsumableTeleportationItems) {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    TELEPORTATION_LEVER("/transports/teleportation_levers.tsv", "useTeleportationLevers", "costTeleportationLevers"),
-    TELEPORTATION_MINIGAME("/transports/teleportation_minigames.tsv", "useTeleportationMinigames", "costTeleportationMinigames") {
+    TELEPORTATION_LEVER("/transports/teleportation_levers.tsv", ShortestPathConfig::useTeleportationLevers, ShortestPathConfig::costTeleportationLevers),
+    TELEPORTATION_MINIGAME("/transports/teleportation_minigames.tsv", ShortestPathConfig::useTeleportationMinigames, ShortestPathConfig::costTeleportationMinigames) {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    TELEPORTATION_PORTAL("/transports/teleportation_portals.tsv", "useTeleportationPortals", "costTeleportationPortals"),
-    TELEPORTATION_PORTAL_POH("/transports/teleportation_portals_poh.tsv", "useTeleportationPortalsPoh", null),
-    TELEPORTATION_SPELL("/transports/teleportation_spells.tsv", "useTeleportationSpells", "costTeleportationSpells") {
+    TELEPORTATION_PORTAL("/transports/teleportation_portals.tsv", ShortestPathConfig::useTeleportationPortals, ShortestPathConfig::costTeleportationPortals),
+    TELEPORTATION_PORTAL_POH("/transports/teleportation_portals_poh.tsv", ShortestPathConfig::useTeleportationPortalsPoh, null),
+    TELEPORTATION_SPELL("/transports/teleportation_spells.tsv", ShortestPathConfig::useTeleportationSpells, ShortestPathConfig::costTeleportationSpells) {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    WILDERNESS_OBELISK("/transports/wilderness_obelisks.tsv", "useWildernessObelisks", "costWildernessObelisks"),
+    WILDERNESS_OBELISK("/transports/wilderness_obelisks.tsv", ShortestPathConfig::useWildernessObelisks, ShortestPathConfig::costWildernessObelisks),
     ;
 
     private final String resourcePath;
-    private final String configKey;
-    private final String costKey;
+    private final Function<ShortestPathConfig, Boolean> enabledGetter;
+    private final Function<ShortestPathConfig, Integer> costGetter;
     private final Integer radiusThreshold;
 
-    TransportType(String resourcePath, String configKey, String costKey) {
-        this(resourcePath, configKey, costKey, null);
+    TransportType(String resourcePath, Function<ShortestPathConfig, Boolean> enabledGetter, Function<ShortestPathConfig, Integer> costGetter) {
+        this(resourcePath, enabledGetter, costGetter, null);
     }
 
-    TransportType(String resourcePath, String configKey, String costKey, Integer radiusThreshold) {
+    TransportType(String resourcePath, Function<ShortestPathConfig, Boolean> enabledGetter, Function<ShortestPathConfig, Integer> costGetter, Integer radiusThreshold) {
         this.resourcePath = resourcePath;
-        this.configKey = configKey;
-        this.costKey = costKey;
+        this.enabledGetter = enabledGetter;
+        this.costGetter = costGetter;
         this.radiusThreshold = radiusThreshold;
     }
 
@@ -84,12 +87,12 @@ public enum TransportType {
         return radiusThreshold != null;
     }
 
-    public boolean hasConfigKey() {
-        return configKey != null;
+    public boolean hasEnabledGetter() {
+        return enabledGetter != null;
     }
 
-    public boolean hasCostKey() {
-        return costKey != null;
+    public boolean hasCostGetter() {
+        return costGetter != null;
     }
 
     /*
