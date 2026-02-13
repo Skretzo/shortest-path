a7ec375f1e4c5f7cde362b62ed701332ad8f2ada
Frans Irgolitsch <f.irgolitsch@outlook.com>
2026-02-12 21:21:29 -0500

    Added config and cost keys to the enum values

--- src/main/java/shortestpath/transport/TransportType.java
+++ src/main/java/shortestpath/transport/TransportType.java
@@ -5,8 +5,8 @@ import net.runelite.api.Skill;
 
 @Getter
 public enum TransportType {
-    TRANSPORT("/transports/transports.tsv"),
-    AGILITY_SHORTCUT("/transports/agility_shortcuts.tsv") {
+    TRANSPORT("/transports/transports.tsv", null, null),
+    AGILITY_SHORTCUT("/transports/agility_shortcuts.tsv", "useAgilityShortcuts", "costAgilityShortcuts") {
         @Override
         public TransportType refine(int[] skillLevels) {
             if (skillLevels[Skill.RANGED.ordinal()] > 1 || skillLevels[Skill.STRENGTH.ordinal()] > 1) {
@@ -15,54 +15,58 @@ public enum TransportType {
             return this;
         }
     },
-    GRAPPLE_SHORTCUT(null),
-    BOAT("/transports/boats.tsv"),
-    CANOE("/transports/canoes.tsv"),
-    CHARTER_SHIP("/transports/charter_ships.tsv"),
-    SHIP("/transports/ships.tsv"),
-    FAIRY_RING("/transports/fairy_rings.tsv"),
-    GNOME_GLIDER("/transports/gnome_gliders.tsv", 6),
-    HOT_AIR_BALLOON("/transports/hot_air_balloons.tsv", 7),
-    MAGIC_CARPET("/transports/magic_carpets.tsv"),
-    MAGIC_MUSHTREE("/transports/magic_mushtrees.tsv", 5),
-    MINECART("/transports/minecarts.tsv"),
-    QUETZAL("/transports/quetzals.tsv"),
-    SEASONAL_TRANSPORTS("/transports/seasonal_transports.tsv"),
-    SPIRIT_TREE("/transports/spirit_trees.tsv", 5),
-    TELEPORTATION_BOX("/transports/teleportation_boxes.tsv"),
-    TELEPORTATION_ITEM("/transports/teleportation_items.tsv") {
+    GRAPPLE_SHORTCUT(null, "useGrappleShortcuts", "costGrappleShortcuts"),
+    BOAT("/transports/boats.tsv", "useBoats", "costBoats"),
+    CANOE("/transports/canoes.tsv", "useCanoes", "costCanoes"),
+    CHARTER_SHIP("/transports/charter_ships.tsv", "useCharterShips", "costCharterShips"),
+    SHIP("/transports/ships.tsv", "useShips", "costShips"),
+    FAIRY_RING("/transports/fairy_rings.tsv", "useFairyRings", "costFairyRings", 6),
+    GNOME_GLIDER("/transports/gnome_gliders.tsv", "useGnomeGliders", "costGnomeGliders", 6),
+    HOT_AIR_BALLOON("/transports/hot_air_balloons.tsv", "useHotAirBalloons", "costHotAirBalloons", 7),
+    MAGIC_CARPET("/transports/magic_carpets.tsv", "useMagicCarpets", "costMagicCarpets"),
+    MAGIC_MUSHTREE("/transports/magic_mushtrees.tsv", "useMagicMushtrees", "costMagicMushtrees", 5),
+    MINECART("/transports/minecarts.tsv", "useMinecarts", "costMinecarts"),
+    QUETZAL("/transports/quetzals.tsv", "useQuetzals", "costQuetzals"),
+    SEASONAL_TRANSPORTS("/transports/seasonal_transports.tsv", "useSeasonalTransports", "costSeasonalTransports"),
+    SPIRIT_TREE("/transports/spirit_trees.tsv", "useSpiritTrees", "costSpiritTrees", 5),
+    TELEPORTATION_BOX("/transports/teleportation_boxes.tsv", null, "costTeleportationBoxes"),
+    TELEPORTATION_ITEM("/transports/teleportation_items.tsv", null, "costNonConsumableTeleportationItems") {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    TELEPORTATION_LEVER("/transports/teleportation_levers.tsv"),
-    TELEPORTATION_MINIGAME("/transports/teleportation_minigames.tsv") {
+    TELEPORTATION_LEVER("/transports/teleportation_levers.tsv", "useTeleportationLevers", "costTeleportationLevers"),
+    TELEPORTATION_MINIGAME("/transports/teleportation_minigames.tsv", "useTeleportationMinigames", "costTeleportationMinigames") {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    TELEPORTATION_PORTAL("/transports/teleportation_portals.tsv"),
-    TELEPORTATION_PORTAL_POH("/transports/teleportation_portals_poh.tsv"),
-    TELEPORTATION_SPELL("/transports/teleportation_spells.tsv") {
+    TELEPORTATION_PORTAL("/transports/teleportation_portals.tsv", "useTeleportationPortals", "costTeleportationPortals"),
+    TELEPORTATION_PORTAL_POH("/transports/teleportation_portals_poh.tsv", "useTeleportationPortalsPoh", null),
+    TELEPORTATION_SPELL("/transports/teleportation_spells.tsv", "useTeleportationSpells", "costTeleportationSpells") {
         @Override
         public boolean isTeleport() {
             return true;
         }
     },
-    WILDERNESS_OBELISK("/transports/wilderness_obelisks.tsv"),
+    WILDERNESS_OBELISK("/transports/wilderness_obelisks.tsv", "useWildernessObelisks", "costWildernessObelisks"),
     ;
 
     private final String resourcePath;
+    private final String configKey;
+    private final String costKey;
     private final Integer radiusThreshold;
 
-    TransportType(String resourcePath) {
-        this(resourcePath, null);
+    TransportType(String resourcePath, String configKey, String costKey) {
+        this(resourcePath, configKey, costKey, null);
     }
 
-    TransportType(String resourcePath, Integer radiusThreshold) {
+    TransportType(String resourcePath, String configKey, String costKey, Integer radiusThreshold) {
         this.resourcePath = resourcePath;
+        this.configKey = configKey;
+        this.costKey = costKey;
         this.radiusThreshold = radiusThreshold;
     }
 
@@ -72,6 +76,14 @@ public enum TransportType {
 
     public boolean hasRadiusThreshold() {
         return radiusThreshold != null;
+    }
+
+    public boolean hasConfigKey() {
+        return configKey != null;
+    }
+
+    public boolean hasCostKey() {
+        return costKey != null;
     }
 
     /*
