7dc7d27529fcfd770978f26ca988008b06dfa15b
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2026-04-16 12:37:27 +0200

    Update unreachable target distance to 2 (down from 20)

--- src/main/java/shortestpath/ShortestPathConfig.java
+++ src/main/java/shortestpath/ShortestPathConfig.java
@@ -353,7 +353,7 @@ public interface ShortestPathConfig extends Config {
         section = sectionSettings
     )
     default int unreachableTargetDistance() {
-        return 20;
+        return 2;
     }
 
     @ConfigItem(
