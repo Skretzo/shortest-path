5ce2b68e323cea1707a890f6ef645d209d067316
jarromie <jarromie@whocodes.tech>
2026-04-19 06:03:22 -0400

    Modify imports to match code conventions

--- src/main/java/shortestpath/ShortestPathConfig.java
+++ src/main/java/shortestpath/ShortestPathConfig.java
@@ -1,8 +1,14 @@
 package shortestpath;
 
 import java.awt.Color;
-
-import net.runelite.client.config.*;
+import net.runelite.client.config.Alpha;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
+import net.runelite.client.config.Range;
+import net.runelite.client.config.Units;
+import net.runelite.client.config.Keybind;
 
 @ConfigGroup(ShortestPathPlugin.CONFIG_GROUP)
 public interface ShortestPathConfig extends Config {
