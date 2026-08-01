90f09a71db43fc1d933af39b49750672a77508e0
Ku Tadao <29869255+Ku-Tadao@users.noreply.github.com>
2026-08-01 18:32:50 +0200

    Prefer shorter paths near unreachable targets

--- src/main/java/shortestpath/pathfinder/PathfinderConfig.java
+++ src/main/java/shortestpath/pathfinder/PathfinderConfig.java
@@ -117,6 +117,8 @@ public class PathfinderConfig
 	@Getter
 	private long calculationCutoffMillis;
 	@Getter
+	private int unreachableTargetDistance;
+	@Getter
 	private boolean avoidWilderness;
 	// POH-specific settings (not tied to a single TransportType)
 	private boolean usePohFairyRing,
@@ -267,6 +269,7 @@ public class PathfinderConfig
 	public void refresh()
 	{
 		calculationCutoffMillis = (long) config.calculationCutoff() * Constants.GAME_TICK_LENGTH;
+		unreachableTargetDistance = ShortestPathPlugin.override("unreachableTargetDistanceThreshold", config.unreachableTargetDistance());
 		avoidWilderness = ShortestPathPlugin.override("avoidWilderness", config.avoidWilderness());
 		usePoh = ShortestPathPlugin.override("usePoh", config.usePoh());
 		leagueModeState.refresh(client);
