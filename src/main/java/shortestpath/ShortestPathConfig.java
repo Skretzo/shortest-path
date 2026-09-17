2314201e332077bbcfa490453042a83b70c24ef8
Matthew Pickering <matthewtpickering@gmail.com>
2026-09-17 09:09:41 +0100

    Migrate POH portal nexus config
    This removes the explicit on/off flag for the portal nexus in favour of
    selecting all the teleports from the multi-select (Ctrl-A).
    
    The default value (all or or all off) is populated from the previous
    user choice before updating the plugin.

--- src/main/java/shortestpath/ShortestPathConfig.java
+++ src/main/java/shortestpath/ShortestPathConfig.java
@@ -557,10 +557,9 @@ public interface ShortestPathConfig extends Config
 
 	@ConfigItem(
 		keyName = "useTeleportationPortalsPoh",
-		name = "POH portal nexus",
-		description = "Whether to include POH teleportation portals/nexus in the path",
-		position = 39,
-		section = sectionPoh
+		name = "",
+		description = "",
+		hidden = true
 	)
 	default boolean useTeleportationPortalsPoh()
 	{
@@ -576,7 +575,9 @@ public interface ShortestPathConfig extends Config
 	)
 	default Set<PohNexusPortal> pohNexusPortals()
 	{
-		return EnumSet.allOf(PohNexusPortal.class);
+		return useTeleportationPortalsPoh()
+			? EnumSet.allOf(PohNexusPortal.class)
+			: EnumSet.noneOf(PohNexusPortal.class);
 	}
 
 	@ConfigItem(
